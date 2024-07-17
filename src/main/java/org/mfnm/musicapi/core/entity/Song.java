27b18f1bd779c23af299db238602f07225ac49da
PedroSmaxY <pedrohnovais@outlook.com>
2024-07-17 18:42:24 -0300

    feat: Add annotations and constraints to Playlist entity
    The code changes include adding annotations and constraints to the Playlist entity class, such as @Entity, @Table, @Id, @Column, @ManyToOne, @JoinColumn, @OneToMany, @NotNull, and @NotEmpty. This improves the data model and ensures data integrity.

--- src/main/java/org/mfnm/musicapi/core/entity/Song.java
+++ src/main/java/org/mfnm/musicapi/core/entity/Song.java
@@ -1,54 +1,168 @@
 package org.mfnm.musicapi.core.entity;
 
-//import javax.persistence.*;
-
 import jakarta.persistence.*;
-import lombok.Data;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
 
 import java.io.Serializable;
 import java.util.Date;
-@Data
-@Entity
-@Table(schema = "music_db", name = "songs")
-public class Song  implements Serializable {
 
+@Entity
+@Table(name = Song.TABLE_NAME)
+public class Song implements Serializable {
+
+    private static final String TABLE_NAME = "song";
     private static final long serialVersionUID = 1l;
 
-
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
-    private long id;
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", unique = true)
+    private Long id;
 
+    @Column(name = "songname", length = 100, nullable = false)
+    @NotNull
+    @NotEmpty
     private String name;
+
+    @Column(name = "artistname", length = 100, nullable = false)
+    @NotNull
+    @NotEmpty
     private String artist;
+
+    @Column(name = "albumname", length = 100, nullable = true)
+    @NotNull
+    @NotEmpty
     private String album;
+
+    @Column(name = "releasedate", nullable = true)
+    @NotEmpty
     private Date releaseDate;
 
-    public long getId() {
-        return id;
+    @ManyToOne
+    @JoinColumn(name = "playlist_id", nullable = false, updatable = true)
+    private Playlist playlist;
+
+    public Song() {
     }
 
-    public Song(int id, String name, String artist, String album, Date releaseDate){
+    public Song(Long id, @NotNull @NotEmpty String name, @NotNull @NotEmpty String artist,
+            @NotNull @NotEmpty String album, @NotEmpty Date releaseDate) {
         this.id = id;
-        this.name = artist;
+        this.name = name;
         this.artist = artist;
         this.album = album;
         this.releaseDate = releaseDate;
     }
 
+    /**
+     * @param id the id to set
+     */
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    /**
+     * @return String return the name
+     */
     public String getName() {
         return name;
     }
 
+    /**
+     * @param name the name to set
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    /**
+     * @return String return the artist
+     */
     public String getArtist() {
         return artist;
     }
 
+    /**
+     * @param artist the artist to set
+     */
+    public void setArtist(String artist) {
+        this.artist = artist;
+    }
+
+    /**
+     * @return String return the album
+     */
     public String getAlbum() {
         return album;
     }
 
-    public Date getReleaseDate(){
-        return  releaseDate;
+    /**
+     * @param album the album to set
+     */
+    public void setAlbum(String album) {
+        this.album = album;
+    }
+
+    /**
+     * @return Date return the releaseDate
+     */
+    public Date getReleaseDate() {
+        return releaseDate;
+    }
+
+    /**
+     * @param releaseDate the releaseDate to set
+     */
+    public void setReleaseDate(Date releaseDate) {
+        this.releaseDate = releaseDate;
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((id == null) ? 0 : id.hashCode());
+        result = prime * result + ((name == null) ? 0 : name.hashCode());
+        result = prime * result + ((artist == null) ? 0 : artist.hashCode());
+        result = prime * result + ((album == null) ? 0 : album.hashCode());
+        result = prime * result + ((releaseDate == null) ? 0 : releaseDate.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Song other = (Song) obj;
+        if (id == null) {
+            if (other.id != null)
+                return false;
+        } else if (!id.equals(other.id))
+            return false;
+        if (name == null) {
+            if (other.name != null)
+                return false;
+        } else if (!name.equals(other.name))
+            return false;
+        if (artist == null) {
+            if (other.artist != null)
+                return false;
+        } else if (!artist.equals(other.artist))
+            return false;
+        if (album == null) {
+            if (other.album != null)
+                return false;
+        } else if (!album.equals(other.album))
+            return false;
+        if (releaseDate == null) {
+            if (other.releaseDate != null)
+                return false;
+        } else if (!releaseDate.equals(other.releaseDate))
+            return false;
+        return true;
     }
 }
--- src/main/java/org/mfnm/musicapi/core/entity/User.java
+++ src/main/java/org/mfnm/musicapi/core/entity/User.java
@@ -1,22 +1,73 @@
 package org.mfnm.musicapi.core.entity;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
+@Entity
+@Table(name = User.TABLE_NAME)
 public class User {
-    private long id;
+    public class CreateUser {
+    }
+
+    public class UpdateUser {
+    }
+
+    public static final String TABLE_NAME = "user";
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", unique = true)
+    private Long id;
+
+    @Column(name = "username", length = 100, nullable = false, unique = true)
+    @NotNull(groups = CreateUser.class)
+    @NotEmpty(groups = CreateUser.class)
+    @Size(min = 2, max = 100)
     private String username;
+
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
+    @Column(name = "password", length = 60, nullable = false)
+    @NotNull(groups = { CreateUser.class, UpdateUser.class })
+    @NotEmpty(groups = { CreateUser.class, UpdateUser.class })
     private String password;
+
+    @Column(name = "email", length = 100, nullable = false, unique = true)
+    @NotEmpty
+    @NotNull
     private String email;
 
-    public User(String username, String password, String email){
+    public User() {
+    }
+
+    public User(Long id,
+            @NotNull(groups = CreateUser.class) @NotEmpty(groups = CreateUser.class) @Size(min = 2, max = 100) String username,
+            @NotNull(groups = { CreateUser.class, UpdateUser.class }) @NotEmpty(groups = { CreateUser.class,
+                    UpdateUser.class }) String password,
+            @NotEmpty @NotNull String email) {
+        this.id = id;
         this.username = username;
         this.password = password;
         this.email = email;
     }
 
-    public long getId() {
+    public static String getTableName() {
+        return TABLE_NAME;
+    }
+
+    public Long getId() {
         return id;
     }
 
-    public void setId(long id) {
+    public void setId(Long id) {
         this.id = id;
     }
 
@@ -42,5 +93,48 @@ public class User {
 
     public void setEmail(String email) {
         this.email = email;
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((id == null) ? 0 : id.hashCode());
+        result = prime * result + ((username == null) ? 0 : username.hashCode());
+        result = prime * result + ((password == null) ? 0 : password.hashCode());
+        result = prime * result + ((email == null) ? 0 : email.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        User other = (User) obj;
+        if (id == null) {
+            if (other.id != null)
+                return false;
+        } else if (!id.equals(other.id))
+            return false;
+        if (username == null) {
+            if (other.username != null)
+                return false;
+        } else if (!username.equals(other.username))
+            return false;
+        if (password == null) {
+            if (other.password != null)
+                return false;
+        } else if (!password.equals(other.password))
+            return false;
+        if (email == null) {
+            if (other.email != null)
+                return false;
+        } else if (!email.equals(other.email))
+            return false;
+        return true;
     }
 }

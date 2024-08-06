ff00e81836bac753d2cd9de54e8c09333134d28e
Pedro Henrique <pedrohnovais@outlook.com>
2024-08-06 01:03:59 -0300

    Security Configurations fixed

--- src/main/java/org/mfnm/musicapi/configs/SecurityConfig.java
+++ src/main/java/org/mfnm/musicapi/configs/SecurityConfig.java
@@ -6,6 +6,7 @@ import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -22,19 +23,17 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        return http
-                .authorizeHttpRequests(
-                        authorizeConfig -> {
-                            authorizeConfig.requestMatchers("/user/**").permitAll();
-                            authorizeConfig.requestMatchers("/song/**").permitAll();
-                            authorizeConfig.requestMatchers("/playlists/**").permitAll();
-                            authorizeConfig.anyRequest().authenticated();
-                        }
-                ).formLogin(Customizer.withDefaults()).build();
+        http
+                .csrf(AbstractHttpConfigurer::disable) // Desabilita CSRF para facilitar o teste (não recomendado para produção)
+                .authorizeHttpRequests(authorizeConfig -> {
+                    authorizeConfig.anyRequest().permitAll();
+                });
+        return http.build();
     }
+
 
     @Bean
     public WebSecurityCustomizer webSecurityCustomizer() {
-        return (web) -> web.ignoring().requestMatchers("/css/**", "/js/**", "/images/**");
+        return web -> web.ignoring().requestMatchers("/css/**", "/js/**", "/images/**");
     }
 }

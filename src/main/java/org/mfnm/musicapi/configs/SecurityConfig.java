6a1606a0ac5eb056ea04a617d4e9679f3e52feda
Pedro Henrique <pedrohnovais@outlook.com>
2024-08-06 00:41:18 -0300

    Security Configurations fixed

--- src/main/java/org/mfnm/musicapi/configs/SecurityConfig.java
+++ src/main/java/org/mfnm/musicapi/configs/SecurityConfig.java
@@ -2,14 +2,13 @@ package org.mfnm.musicapi.configs;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
-
-import static org.springframework.security.config.Customizer.withDefaults;
 
 
 @Configuration
@@ -23,14 +22,15 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http
-                .csrf(csrf -> csrf.disable()) // Desabilita CSRF
-                .authorizeHttpRequests(authorize -> authorize
-                        .anyRequest().permitAll() // Permite acesso público a todas as rotas
-                )
-                .httpBasic(); // Usa autenticação básica HTTP
-
-        return http.build();
+        return http
+                .authorizeHttpRequests(
+                        authorizeConfig -> {
+                            authorizeConfig.requestMatchers("/user/**").permitAll();
+                            authorizeConfig.requestMatchers("/song/**").permitAll();
+                            authorizeConfig.requestMatchers("/playlists/**").permitAll();
+                            authorizeConfig.anyRequest().authenticated();
+                        }
+                ).formLogin(Customizer.withDefaults()).build();
     }
 
     @Bean

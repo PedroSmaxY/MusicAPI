85ed67b97ce5d51c359b4e6085475fe046fa68d5
Pedro Henrique <pedrohnovais@outlook.com>
2024-08-06 21:59:40 -0300

    Refactored

--- src/main/java/org/mfnm/musicapi/domain/user/User.java
+++ src/main/java/org/mfnm/musicapi/domain/user/User.java
@@ -5,6 +5,7 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 
 import com.fasterxml.jackson.annotation.ObjectIdGenerators;
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
@@ -40,21 +41,18 @@ public class User {
     private Long id;
 
     @Column(name = "username", length = 100, nullable = false, unique = true)
-    @NotNull(groups = CreateUser.class)
-    @NotEmpty(groups = CreateUser.class)
+    @NotBlank(groups = CreateUser.class)
     @Size(min = 2, max = 100)
     private String username;
 
     @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
     @Column(name = "password", length = 60, nullable = false)
-    @NotNull(groups = {CreateUser.class, UpdateUser.class})
-    @NotEmpty(groups = {CreateUser.class, UpdateUser.class})
+    @NotBlank(groups = {CreateUser.class, UpdateUser.class})
     private String password;
 
     @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
     @Column(name = "email", length = 100, nullable = false, unique = true)
-    @NotEmpty
-    @NotNull
+    @NotBlank
     private String email;
 
     @OneToMany(mappedBy = "user", cascade = CascadeType.REMOVE, orphanRemoval = true)

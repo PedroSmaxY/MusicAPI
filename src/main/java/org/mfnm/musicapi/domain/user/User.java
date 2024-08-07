e0d4c9c3de95ae162335a05e3851ca4d6b18fcee
Pedro Henrique <pedrohnovais@outlook.com>
2024-08-06 22:28:20 -0300

    Refactored

--- src/main/java/org/mfnm/musicapi/domain/user/User.java
+++ src/main/java/org/mfnm/musicapi/domain/user/User.java
@@ -6,8 +6,6 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.ObjectIdGenerators;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotEmpty;
-import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
